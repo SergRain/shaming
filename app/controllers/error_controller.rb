22d186df13e8e436e17067c9216528fcebfd2205
SergRain <sr@zestvisual.com>
2022-07-09 19:19:37 +0300

    fix utm in url for statis

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -2,7 +2,7 @@ class ErrorController < ApplicationController
   def not_found
     request.original_fullpath
     #find static file
-    render inline: File.read("#{Rails.root}/public/static/#{request.original_fullpath.split("/").last}")
+    render inline: File.read("#{Rails.root}/public/static/#{request.original_fullpath.split("/").last.split("?")[0]}")
     #render json: request.original_fullpath
   end
 end
